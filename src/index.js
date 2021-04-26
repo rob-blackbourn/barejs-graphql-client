8b511ce35cc50d4b61c0ab75fc7bcfce9f843568
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-04-26 15:08:47 +0100

    Exported more errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import EventError from './event-error'
 import FetchError from './fetch-error'
+import GraphQLError from './graphql-error'
 import graphqlEventSourceClient from './graphql-event-source-client'
 import graphqlEventSourceSubscriber from './graphql-event-source-subscriber'
 import graphqlFetchClient from './graphql-fetch-client'
@@ -7,7 +9,9 @@ import graphqlWsSubscriber from './graphql-ws-subscriber'
 import graphqlWsClient from './graphql-ws-client'
 
 export {
+  EventError,
   FetchError,
+  GraphQLError,
   graphqlEventSourceClient,
   graphqlEventSourceSubscriber,
   graphqlFetchClient,
